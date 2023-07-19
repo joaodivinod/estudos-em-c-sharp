16dcc76f17655dd8a90dfc8af49d218dbcd08cf7
JOAO DIVINO DA SILVA <60980853+joaodivinod@users.noreply.github.com>
2023-07-18 23:26:29 -0300

    variveis e condicionais

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,3 +1,53 @@
 ﻿// Screen Sound
 string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+
+void ExibirMensagemDeBoasVindas()
+{
+  Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+}
+
+
+void ExibirOpcoesDoMenu()
+{
+  Console.WriteLine("\nDigite 1 para registrar uma banda");
+  Console.WriteLine("Digite 2 para mostrar Todas as Bandas");
+  Console.WriteLine("Digite 3 Avaliar uma banda");
+  Console.WriteLine("Digite 4 Para exibir a media de uma banda");
+  Console.WriteLine("Digite -1 para sair");
+
+  Console.Write("\nDigite e sua opção ");
+  String OpcaoEscolida = Console.ReadLine()!;
+  int OpcaoEscolidaNumerica = int.Parse(OpcaoEscolida);
+
+  switch (OpcaoEscolidaNumerica)
+  {
+    case 1: Console.WriteLine("você escolheu a opção " + OpcaoEscolidaNumerica);
+      break;
+    case 2:
+      Console.WriteLine("você escolheu a opção " + OpcaoEscolidaNumerica);
+      break;
+    case 3:
+      Console.WriteLine("você escolheu a opção " + OpcaoEscolidaNumerica);
+      break;
+    case 4:
+      Console.WriteLine("você escolheu a opção " + OpcaoEscolidaNumerica);
+      break;
+    case -1:
+      Console.WriteLine("você escolheu a opção"  + OpcaoEscolidaNumerica);
+      break;
+    default: Console.WriteLine("Num intindi o que você escreveu");
+      break;
+  }
+}
+
+ExibirMensagemDeBoasVindas();
 Console.WriteLine(mensagemDeBoasVindas);
+ExibirOpcoesDoMenu();
